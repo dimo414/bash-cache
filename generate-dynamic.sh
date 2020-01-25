14c148b4dc76a99197c598c56450a005df891065
Michael Diamond <dimo414@gmail.com>
2020-01-24 16:20:08 -0800

    Generate the script to check, rather than checking it directly, and swap to koalaman/shellcheck-alpine to check both the checked-in and generated code.

---
+++ generate-dynamic.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Creates a script containing samples of all the dynamically generated
+# functions bash-cache.sh makes. The resulting script can be used to lint and
+# validate this dynamic content (namely via ShellCheck).
+#
+# Note that inline comments are not preserved making it difficult to suppress
+# warnings. It may be necessary to manipulate the generated script before
+# invoking shellcheck.
+
+DIR=$(mktemp -d "${TMPDIR:-/tmp}/bc-benchmark-XXXXXX")
+trap 'rm -rf "$DIR"' EXIT
+
+# https://stackoverflow.com/a/246128/113632
+# shellcheck source=/dev/null
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/bash-cache.sh"
+
+declare -F | cut -d' ' -f3 | sort > "$DIR/orig_func.txt"
+
+# Since bc::locked_cache delegates to bc::cache this should give us coverage of
+# All dynamically generated functions
+expensive() { :; } && bc::locked_cache expensive
+
+printf "Capturing generated functions:"
+printf '#!/bin/bash\n#\n# GENERATED SCRIPT FOR USE WITH SHELLCHECK\n' > "generated.sh"
+# shellcheck disable=SC2016
+printf '_bc_cache_dir=$1\n_bc_enabled=$2\n' >> "generated.sh" # declare config variables
+for func in $(comm -13 "$DIR/orig_func.txt" <(declare -F | cut -d' ' -f3 | sort)); do
+  printf " %s" "$func"
+  echo >> "generated.sh"
+  type "$func" | tail -n+2 >> "generated.sh"
+done
+printf '\nWrote generated functions to generated.sh\n'
